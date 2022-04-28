d594ed5de2e913b026ceba7c8c09c35b0bafb1d7
Tomer <44266242+TomerGamerTV@users.noreply.github.com>
2022-04-28 22:51:21 +0300

    Add files via upload
    https://www.roblox.com/games/54865335/Ultimate-Driving

---
+++ ultimate driving eggfarm 2022 by me.lua
@@ -0,0 +1,59 @@
+-- Made By TomerGamerTV
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⡤⡤⠄⠀⠀⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣷⣿⣿⣿⣿⣿⣿⣿⡿⣽⣟⣿⡿⣿⣞⣯⣻⣷⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣟⡿⣿⣿⣿⣿⣿⣯⣺⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠈⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠛⠛⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-677.3043212890625, 428.8070983886719, -4932.396484375)
+wait(3)
+print("1")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1815.4443359375, 1.0499995946884155, 8364.5625)
+wait(3)
+print("2")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(905.5172729492188, 445.5585632324219, 6321.2998046875)
+wait(3)
+print("3")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-14425.822265625, 6.169576644897461, -5389.3984375)
+wait(3)
+print("4")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1820.1824951171875, 105.2968521118164, 3104.161865234375)
+wait(3)
+print("5")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2002.1265869140625, 36.9999885559082, 5792.3994140625)
+wait(3)
+print("6")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-824.8517456054688, -3.0000007152557373, 6887.1396484375)
+wait(3)
+print("7")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-386.6870422363281, 512.432861328125, 8811.677734375)
+wait(3)
+print("8")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(520.352294921875, 571.90771484375, 8923.759765625)
+wait(3)
+print("9")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-33.59560775756836, 167.123291015625, 8364.0107421875)
+wait(3)
+print("10")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(304.8334655761719, 250.5496063232422, 8587.650390625)
+wait(3)
+print("11")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1513.4073486328125, 58.82500076293945, 5330.57666015625)
+wait(3)
+print("12")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5285.32080078125, 180.75588989257812, 4057.155517578125)
+wait(3)
+print("13")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2603.13818359375, 2.518634080886841, 1745.14892578125)
+wait(3)
+print("14")
